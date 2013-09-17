f61611f97b27b3d581d46365b0279ecd4e9cac00
Kevin Solorio <soloriok@gmail.com>
2013-09-17 13:58:14 -0500

    added new

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,22 @@
+# GET ========================================================
+
 get '/' do
-  @contacts = []
+  @contacts = Contact.order('last_name ASC')
   erb :index
+end
+
+get '/contacts/new' do
+  @contact = Contact.new(params[:contact])
+  erb :new
+end
+
+# POST ========================================================
+
+post '/contacts' do
+  @contact = Contact.new(params[:contact])
+  if @contact.save
+    redirect to('/')
+  else
+    erb :new
+  end
 end
